af35d695eaec58086c71eb31d6dfd056f40386d1
ahmed helmy <ahmedhelmy200@gmail.com>
2026-05-15 21:23:18 +0400

    Install portal data bridge in HELM Smart entrypoint

--- index.tsx
+++ index.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { installPortalBridge } from "./services/portalBridge";
 
 // PWA: register service worker on web builds (skip file:// protocol for Electron)
 import { registerSW } from "virtual:pwa-register";
+
+installPortalBridge();
 
 const isElectron = /electron/i.test(navigator.userAgent);
 if ("serviceWorker" in navigator && window.location.protocol !== "file:" && !isElectron) {
